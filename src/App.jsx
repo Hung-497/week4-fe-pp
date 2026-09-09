6df4275fe8b893233ca55cde869368fb0738895a
Hung-497 <giahung092007@gmail.com>
2026-09-09 16:02:23 +0300

    fix(react): nest routes under shared layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import About from "./components/About";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Header from "./components/Header";
 import Services from "./components/Services";
 import Tours from "./components/Tours";
 import Home from "./components/Home";
@@ -14,18 +11,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Hero />
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/" element={<Layout />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/tours" element={<Tours />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/tours" element={<Tours />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
